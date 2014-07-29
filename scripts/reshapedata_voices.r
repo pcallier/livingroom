3153d5c733c14de29437202ea92c4b0d0051d646
Patrick Callier <pcallier@stanford.edu>
2014-07-29 13:00:42 -0700

    Made more changes to voices batch, including looping over fields in metadata. Added more documentation.

---
+++ scripts/reshapedata_voices.r
@@ -0,0 +1,54 @@
+# reshapedata.r
+# Some reworking and retooling of the input data file (1st arg)
+# saved to path given in 2nd arg.
+#
+# Changes table from long to wide (making it much smaller), adds on
+# information at word-, IP-level, including mean median and SD for a bunch of 
+# different measures
+
+library(reshape2)
+library(plyr)
+library(ggplot2)
+library(lme4)
+library(lmerTest)
+
+args <- commandArgs(trailingOnly = TRUE)
+input.path <- args[1]
+output.path <- args[2]
+stopifnot(length(args) >= 2)
+
+data.df <- read.delim(input.path, na.strings="--undefined--", colClasses=c(word_segments="character"))
+data.df$speaker_id <- data.df$speaker
+data.df$phone_id <- paste(data.df$speaker_id, data.df$session_id, data.df$phone, floor(data.df$start_phone * 1000), sep="_")
+data.df$word_id <- with(data.df, paste(speaker_id, session_id, word, floor(start_word * 1000), sep="_"))
+data.df$ip_id <- with(data.df, paste(speaker_id, session_id, floor(start_ip * 1000), sep="_"))
+massive.df <- dcast(data.df, ... ~ Measure, value.var="Value")
+
+phone.df <- dcast(massive.df, phone_id + phone + start_phone + word_id + start_word + word_segments + ip_id + start_ip + end_ip~ ., value.var="")
+word.df <- dcast(phone.df[order(phone.df$start_phone),], word_id + start_word + ip_id + start_ip + end_ip + word_segments ~ "stress_pattern", 
+                 value.var="word_segments", fun.aggregate=function(x) { return(gsub("[^012]", "", x[1])) })
+word.df$num_syls <- sapply(word.df$stress_pattern,nchar)
+word.df$word_in_ip <- with(word.df, ave(start_word, ip_id, FUN=rank))
+word.df$word_in_ip_perct <- with(word.df, (start_word-start_ip)/(end_ip-start_ip))
+
+massive.df <- merge(massive.df, subset(word.df, select=c(word_id,num_syls,word_in_ip,word_in_ip_perct,stress_pattern)),by="word_id")
+
+ip.df <- ddply(massive.df, .(ip_id), .fun=function(x) { 
+  x$F0_max <- max(x$F0,na.rm=TRUE)
+  x$F0_min <- min(x$F0,na.rm=TRUE)
+  x$F0_median <- median(x$F0,na.rm=TRUE)
+  x$F0_mean <- mean(x$F0,na.rm=TRUE)
+  x$F0_sd <- sd(x$F0,na.rm=TRUE)
+  
+  x$int_max <- max(x$intensity,na.rm=TRUE)
+  x$int_min <- min(x$intensity,na.rm=TRUE)
+  x$int_median <- median(x$intensity,na.rm=TRUE)
+  x$int_mean <- mean(x$intensity,na.rm=TRUE)
+  x$int_sd <- sd(x$intensity,na.rm=TRUE)
+  
+  x$ip_syls <- sum(unique(subset(x, select=c(word_id,num_syls)))$num_syls)
+  return(x)
+})
+
+# TODO: word frequency, PVI
+write.table(massive.df, file=output.path, row.names=FALSE, sep="\t")
--- scripts/voices_batch.sh
+++ scripts/voices_batch.sh
@@ -1,51 +1,74 @@
-PROJECT_ROOT=./
-SPEAKER=RED_Fowler_Ginger
-BIGWAV=/Users/BigBrother/Documents/VoCal/Retreat_Sample/RED_Fowler_Ginger.wav
-TRSDATA=${PROJECT_ROOT}/more/${SPEAKER}.txt
-TGFILE=/Users/BigBrother/Documents/VoCal/Retreat_Sample/RED_Fowler_Ginger.TextGrid
-# this is this path from the (Praat) script location to the project root, because Praat sets its working directory to script location
-SCRIPT_TO_ROOT=../.. #TODO: this is a problem... relative paths won't work without this but this breaks absolute paths. Do some manipulation intelligently based on whetehr args are rel or absolute
-# this is the WORKING folder for TGs, not the original location (which is in TGFILE already)
-TGWORKING=/Volumes/Surfer/users/pcallier/tgs/
-WAVWORKING=/Volumes/Surfer/users/pcallier/wavs/
-WAVWORKINGREMOTE=/afs/.ir/users/p/c/pcallier/private/livingroom-util/wav_working/
-# strip Interviewer information from TG
-STRIPPEDTG=${TGWORKING}/${SPEAKER}.TextGrid
-/Applications/Praat.app/Contents/MacOS/Praat ${PROJECT_ROOT}/livingroom/scripts/vocal_strip_interviewer.praat "${TGFILE}" "$STRIPPEDTG"
-# add utterance info to TG
-/Applications/Praat.app/Contents/MacOS/Praat ${PROJECT_ROOT}/livingroom/scripts/add_utterances_to_tg.praat "$TRSDATA" "$STRIPPEDTG" 1
-# put chopped-up audio data onto remote
-# this requires ssh authentication to my Stanford account, and for the AFS file
-# system to be mounted. We're working on making this less messy
-# Chop up WAV into bits (phrase-sized)
-/Applications/Praat.app/Contents/MacOS/Praat ${PROJECT_ROOT}/livingroom/save_labeled_intervals_to_wav_sound_files.praat "$BIGWAV" "$STRIPPEDTG" "$WAVWORKINGREMOTE" 1 0 0 3 0 1 0.025 
-# Creak detection
-# copy creak-related scripts to remote (sigh)
-scp -r ${PROJECT_ROOT}/livingroom/scripts/creak_segmentation pcallier@cardinal.stanford.edu:~/private/livingroom-util/
-ssh pcallier@corn.stanford.edu <<ENDSSH
-	sleep 5
-	matlab -r "cd private/covarep; startup; cd ~/private/livingroom-util/creak_segmentation/; do_creak_detection('../wav_working/'); exit"
-	rm -rf ~/private/livingroom-util/creak_segmentation
-ENDSSH
-# decorate big textgrid with creak
-/Applications/Praat.app/Contents/MacOS/Praat ${PROJECT_ROOT}/livingroom/scripts/creak_segmentation/add_creak_info_to_tg.praat "$STRIPPEDTG" "$WAVWORKINGREMOTE" 4
-# Clean up
-find ${WAVWORKINGREMOTE} -type f -delete
+#!/bin/bash
+# This batch script runs an analysis pipeline for the Voices of
+# California data, retrieving measurements relevant for nasality
+# and voice quality projects, among others.
+#
+# Arguments:	metadata_path -	location of a table with metadata on speakers to analyze. 
+#							 	see project root for headers with what fields it needs to have.
+#				project_root - 	the folder that contains the folder scripts
+#				tg_working -	a working folder for holding the modified alignments textgrid
+#				wav_working -	a working folder for small file chunks
+#
 
-# split again, now for analysis (phone-sized)
-# Praat's deleting takes too long
-find ${WAVWORKING} -type f -delete
-/Applications/Praat.app/Contents/MacOS/Praat ${PROJECT_ROOT}/livingroom/save_labeled_intervals_to_wav_sound_files.praat "$BIGWAV" "$STRIPPEDTG" "$WAVWORKING" 1 0 0 1 1 1 0.025 
-# run analysis script
-if [ $GENDER="male" ]; then
-	GENDERNUMBER=1
-else
-	GENDERNUMBER=2
-fi	
-# then analyze
-/Applications/Praat.app/Contents/MacOS/Praat ${PROJECT_ROOT}/livingroom/PraatVoiceSauceImitator.praat "$SPEAKER" "$WAVWORKING" .wav  "$WAVWORKING" .TextGrid "${PROJECT_ROOT}/results" 1 0.025 0.025 0.010 10 "$GENDERNUMBER" 500 550 1485 1650 2475 2750
-# the results end up in the directory we specify with the name SPEAKER_pitchresults.txt
-RESULTS_PATH=${PROJECT_ROOT}/results/${SPEAKER}_pitchresults.txt
+METADATA_PATH=`cd $1; pwd`
+PROJECT_ROOT=`cd $2; pwd`
+TGWORKING=`cd $3; pwd`
+WAVWORKING=`cd $4; pwd`
+# if you need to change the remote working location, just change the script since other things
+# (the matlab invocation in particular) will also need to change
+WAVWORKINGREMOTE=/afs/.ir/users/p/c/pcallier/private/livingroom-util/wav_working/
+
+
+while read SPEAKER GENDER LOCATION AGE RACE SEXUAL_ORIENTATION WAV_PATH TG_PATH TRS_DATA_PATH; do
+	BIGWAV=$WAV_PATH
+	TRSDATA=$TRS_DATA_PATH
+	TGFILE=$TG_PATH
+	# this is the WORKING folder for TGs, not the original location (which is in TGFILE already)
+	# strip Interviewer information from TG
+	STRIPPEDTG=${TGWORKING}/${SPEAKER}.TextGrid
+	/Applications/Praat.app/Contents/MacOS/Praat ${PROJECT_ROOT}/livingroom/scripts/vocal_strip_interviewer.praat "${TGFILE}" "$STRIPPEDTG"
+	# add utterance info to TG
+	/Applications/Praat.app/Contents/MacOS/Praat ${PROJECT_ROOT}/livingroom/scripts/add_utterances_to_tg.praat "$TRSDATA" "$STRIPPEDTG" 1
+	# put chopped-up audio data onto remote
+	# this requires ssh authentication to my Stanford account, and for the AFS file
+	# system to be mounted. We're working on making this less messy
+	# Chop up WAV into bits (phrase-sized)
+	/Applications/Praat.app/Contents/MacOS/Praat ${PROJECT_ROOT}/livingroom/save_labeled_intervals_to_wav_sound_files.praat "$BIGWAV" "$STRIPPEDTG" "$WAVWORKINGREMOTE" 1 0 0 3 0 1 0.025 
+	# Creak detection
+	# copy creak-related scripts to remote (sigh)
+	scp -r ${PROJECT_ROOT}/livingroom/scripts/creak_segmentation pcallier@cardinal.stanford.edu:~/private/livingroom-util/
+	ssh pcallier@corn.stanford.edu <<ENDSSH
+		sleep 5
+		matlab -r "cd private/covarep; startup; cd ~/private/livingroom-util/creak_segmentation/; do_creak_detection('../wav_working/'); exit"
+		rm -rf ~/private/livingroom-util/creak_segmentation
+ENDSSH
+	# decorate big textgrid with creak
+	/Applications/Praat.app/Contents/MacOS/Praat ${PROJECT_ROOT}/livingroom/scripts/creak_segmentation/add_creak_info_to_tg.praat "$STRIPPEDTG" "$WAVWORKINGREMOTE" 4
+	# Clean up
+	find ${WAVWORKINGREMOTE} -type f -delete
+
+	# split again, now for analysis (phone-sized)
+	# Praat's deleting takes too long
+	find ${WAVWORKING} -type f -delete
+	/Applications/Praat.app/Contents/MacOS/Praat ${PROJECT_ROOT}/livingroom/save_labeled_intervals_to_wav_sound_files.praat "$BIGWAV" "$STRIPPEDTG" "$WAVWORKING" 1 0 0 1 1 1 0.025 
+	# run analysis script
+	if [ $GENDER="male" ]; then
+		GENDERNUMBER=1
+	else
+		GENDERNUMBER=2
+	fi	
+	# then analyze
+	/Applications/Praat.app/Contents/MacOS/Praat ${PROJECT_ROOT}/livingroom/PraatVoiceSauceImitator.praat "$SPEAKER" "$WAVWORKING" .wav  "$WAVWORKING" .TextGrid "${PROJECT_ROOT}/results" 1 0.025 0.025 0.010 10 "$GENDERNUMBER" 500 550 1485 1650 2475 2750
+	# the results end up in the directory we specify with the name SPEAKER_pitchresults.txt
+	MEASUREMENTS_PATH=${PROJECT_ROOT}/results/${SPEAKER}_pitchresults.txt
+	MEASUREMENTS_DECORATED_PATH=${PROJECT_ROOT}/results/${SPEAKER}_pitchresults_decorated.txt
+	MEASUREMENTS_RESHAPED_PATH=${PROJECT_ROOT}/results/${SPEAKER}_pitchresults_reshaped.txt
+	# Decorate
+	/Applications/Praat.app/Contents/MacOS/Praat ${PROJECT_ROOT}/livingroom/decoratedata_voices.praat "${MEASUREMENTS_PATH}" "${STRIPPEDTG}" "${METADATA_PATH}" "${MEASUREMENTS_DECORATED_PATH}"
+	rscript ${PROJECT_ROOT}/scripts/reshapedata_voices.r "$MEASUREMENTS_DECORATED_PATH" "$MEASUREMENTS_RESHAPED_PATH"
+done <$METADATA_PATH
+
+# concatenate all the results
 
 
 #/Applications/Praat.app/Contents/MacOS/Praat /Volumes/Surfer/users/pcallier/livingroom/scripts/save_labeled_intervals_to_wav_sound_files.praat /Users/BigBrother/Documents/VoCal/Retreat_Sample/RED_Fowler_Ginger.wav /Volumes/Surfer/users/pcallier/tgs/RED_Fowler_Ginger.TextGrid /Volumes/Surfer/users/pcallier/wavs/ 1 0 0 1 1 1 0.025 
