7493299fe46aadc7405a12ffa431301e2bceaecf
BigBrother <BigBrother@bigbrothermacpro.stanford.edu>
2014-11-11 17:21:10 -0800

    Updated vowel plotting, still tooling around

--- scripts/toy-analysis.r
+++ scripts/toy-analysis.r
@@ -24,14 +24,17 @@ vowels.df <- merge(ddply(select(droplevels(data.df[grepl("[AEIOU].[01]",data.df$
                                                                           F2=median(.$F2,na.rm=TRUE))) } ),
                    phones.summary, by.x="Segment.label", by.y="row.names")
 
-ggplot(aes(x = F2, y=F1, label=Segment.label), data=filter(vowels.df, count > 10)) +
-  geom_text() + scale_x_reverse() + scale_y_reverse()
+phones.df <- merge(data.df, phones.summary, by.x="Segment.label", by.y="row.names")
 
+ggplot(aes(x = F2, y=F1, color=Segment.label), data=filter(phones.df, count > 10 & grepl("(AA|OW|UW|IY)1",Segment.label))) +
+   geom_contour(breaks=4, stat="density2d") + scale_x_reverse() + scale_y_reverse() + 
+   geom_text(aes(x=F2, y=F1, label=Segment.label), color="black", data=filter(vowels.df, count > 10 & grepl("[AEIOU].[1]",Segment.label)))
+   
 # voice quality first steps
 ggplot(aes(x = Segment.label, y=H1c-H2c), data=data.df) + geom_boxplot()
 ggplot(aes(x = Segment.label, y=CPPS), data=data.df) +  geom_boxplot()
 ggplot(aes(x = CPPS, y=F0), data=data.df) + 
-  stat_density2d() + geom_point(alpha=0.2) 
-ggplot(aes(x = CPPS, y=H1c-H2c), data=massive.df) + 
-  stat_density2d() + geom_point(alpha=0.2) 
+   geom_point(alpha=0.2) + stat_density2d() 
+ggplot(aes(x = CPPS, y=H1c-H2c), data=data.df) + 
+	geom_point(alpha=0.2)  + stat_density2d()
 
